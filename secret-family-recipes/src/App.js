559f689946923bdf85ab9af913bb6ec5780f0af7
Liz Drumm <lizdrumm@gmail.com>
2020-09-21 22:25:24 +0000

    Initial commit

---
+++ secret-family-recipes/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+//import logo from './logo.svg';
+import './App.css';
+import RecipeForm from './components/RecipeForm'
+
+
+
+export default function App(){
+  
+
+      return (
+              <div className="App">
+                <header className="App-header">
+                <header><h1>Welcome to our app!</h1></header>
+          <p> Log in and then submit a recipe after you're in!</p>        
+            <RecipeForm/>
+      
+
+      </header>
+    </div>
+  );
+      }
+
+
